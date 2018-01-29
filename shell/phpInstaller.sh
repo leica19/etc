d351dc190110e13f088c5dfe258a31643d6f4a5a
shigaki daichi <spike-19@ezweb.ne.jp>
2018-01-29 10:43:53 +0900

    Delete phpInstaller.sh

--- shell/phpInstaller.sh
+++
@@ -1,4 +0,0 @@
-
-sudo yum -y install epel-release
-sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
-sudo yum -y install --enablerepo=remi,remi-php70 php php-devel php-mbstring php-pdo php-gd php-openssl php-xml
